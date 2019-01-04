f280277fdea4ccc5179c23eab16d6973990ce8d5
wanze <a358003542@gmail.com>
2019-01-04 10:54:02 +0800

    add database

--- hello.py
+++ hello.py
@@ -1,19 +1,49 @@
 #!/usr/bin/env python
 # -*-coding:utf-8-*-
 
+import os
 from datetime import datetime
-from flask import Flask, render_template
+
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '509e94(ryp$86jni9hgprra1y98gz^og9p3n9(nr(@b@gjbs7&'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(FlaskForm):
@@ -23,12 +53,20 @@ class NameForm(FlaskForm):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow(),
+                           known=session.get('known', False))
 
 
 @app.errorhandler(404)
@@ -39,3 +77,8 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
---
+++ init_db_data.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+
+
+from hello import Role, User, db
+
+admin_role = Role(name='Admin')
+
+mod_role = Role(name='Moderator')
+
+user_role = Role(name='User')
+
+user_john = User(username='john', role=admin_role)
+
+user_susan = User(username='susan', role=user_role)
+
+user_david = User(username='david', role=user_role)
+
+db.session.add(admin_role)
+db.session.add(mod_role)
+db.session.add(user_role)
+db.session.add(user_david)
+db.session.add(user_john)
+db.session.add(user_susan)
+
+db.session.commit()
+
+admin_role.name = 'Administrator'
+db.session.add(admin_role)
+db.session.commit()
+
+db.session.delete(mod_role)
+db.session.commit()
