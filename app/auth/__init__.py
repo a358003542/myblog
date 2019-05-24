3e9c4d1f781c742adef59fbfc95c7ccc61103376
wanze <a358003542@gmail.com>
2019-05-24 15:52:15 +0800

    go

--- app/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*-coding:utf-8-*-
-
-
-from flask import Blueprint
-
-auth = Blueprint('auth', __name__)
-
-from . import views
--- init_db.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*-coding:utf-8-*-
-
-
-from myblog import db
-
-db.create_all()
